6d92ab2aba0c18f693a6bf212fe8e0f8268bd766
Alexandr Korsak <alex.korsak@gmail.com>
2012-02-02 00:37:58 +0300

    Add the second level definition

--- src/models/levels.js
+++ src/models/levels.js
@@ -5,10 +5,18 @@ Levels.generate = function(game) {
 
   var level01 = function() {
     return new Level(game, {
-      name: 'Tutorial Level',
+      name: 'Level 1',
       map: [[0, 0, 0, 0, 1, 1, 0, 0, 0, 0]]
     });
   };
 
+  var level02 = function() {
+    return new Level(game, {
+      name: 'Level 2',
+      map: [[0, 1, 0, 0, 1, 1, 0, 0, 1, 0]]
+    });
+  };
+
+  Levels.levels.push(level02);
   Levels.levels.push(level01);
 };
