f2d4879a9a151fe56f1ed272dc336a57e0b9aaf4
Alexandr Korsak <alex.korsak@gmail.com>
2012-02-05 22:38:37 +0300

    Change animation selector

--- public/src/base/animation.js
+++ public/src/base/animation.js
@@ -1,21 +1,20 @@
 (function(global) {
   var onEachFrame;
+  // shim layer with setTimeout fallback
+  window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame    ||
+      window.oRequestAnimationFrame      ||
+      window.msRequestAnimationFrame     ||
+      function (callback) {
+        window.setTimeout(callback, 1000 / 60);
+      };
+  })();
 
-  if (window.webkitRequestAnimationFrame) {
-    onEachFrame = function(callback) {
-      var _cb = function() { callback(); webkitRequestAnimationFrame(_cb); };
-      _cb();
-    };
-  } else if (window.mozRequestAnimationFrame) {
-    onEachFrame = function(callback) {
-      var _cb = function() { callback(); mozRequestAnimationFrame(_cb); };
-      _cb();
-
-    }
-  } else {
-    onEachFrame = function(callback) {
-      setInterval(callback, 1000 / 60);
-    }
+  onEachFrame = function(callback) {
+    var _cb = function() { callback(); webkitRequestAnimationFrame(_cb); };
+    _cb();
   };
 
   global.onEachFrame = onEachFrame;
